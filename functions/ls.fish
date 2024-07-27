ee9067c49f46684077ba7e6f776f80613af52866
Cooper b. Anderson <andersc7@rose-hulman.edu>
2024-07-27 17:31:41 -0500

    Add better exa alias

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,3 +1,8 @@
-function ls --wraps='exa -l' --description 'alias ls exa -l'
-  exa -l $argv;
+function ls --wraps='exa -l --git' --description 'alias ls exa -l --git'
+  # exa -l --group-directories-first --git --git-ignore $argv;
+  exa -l --group-directories-first --git $argv;
+end
+
+function la --wraps='exa -l --git' --description 'alias ls exa -l --git'
+  exa -la --group-directories-first --git $argv;
 end
