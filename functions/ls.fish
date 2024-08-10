cc64ff928d35f3d507a9ba0976bd9b9ccbe39ca5
Cooper b. Anderson <cooper@cooperuser.dev>
2024-08-09 19:52:15 -0500

    Switch from exa to eza

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,8 +1,8 @@
-function ls --wraps='exa -l --git' --description 'alias ls exa -l --git'
-  # exa -l --group-directories-first --git --git-ignore $argv;
-  exa -l --group-directories-first --git $argv;
+function ls --wraps='eza -l --git' --description 'alias ls eza -l --git'
+  # eza -l --group-directories-first --git --git-ignore $argv;
+  eza -l --group-directories-first --git $argv;
 end
 
-function la --wraps='exa -l --git' --description 'alias ls exa -l --git'
-  exa -la --group-directories-first --git $argv;
+function la --wraps='eza -l --git' --description 'alias ls eza -l --git'
+  eza -la --group-directories-first --git $argv;
 end
